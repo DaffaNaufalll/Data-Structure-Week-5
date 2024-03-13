0ebb6406afa0f55fbfd978e71fc4268402ca9450
DaffaNaufalll <145669088+DaffaNaufalll@users.noreply.github.com>
2024-03-13 16:14:13 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        ListStack<String> mystackarray = new ListStack<String>();
+        mystackarray.push("A");
+        mystackarray.push("B");
+        mystackarray.push("C");
+        mystackarray.pop();
+        mystackarray.push("D");
+        mystackarray.push("E");
+        mystackarray.pop();
+
+        System.out.println(mystackarray.toString());
+    }
+   
+
+    
+}
